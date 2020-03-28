0c5636ce4261da9f72dd82b068ef2102ed369168
khalidm53 <khalid-muhammad@live.com>
2020-03-28 09:17:48 -0400

    trying to connect to heroku

--- server.js
+++ server.js
@@ -1,76 +1,92 @@
+require('dotenv').config()
 const express = require("express");
 const logger = require("morgan");
+const mongojs = require('mongojs')
 const mongoose = require("mongoose");
-const path = require("path");
-const db = require("./Models");
-const PORT = process.env.PORT || 8080;
+const path = require('path')
 
+
+
+const PORT = process.env.PORT || 3000;
+
+const User = require("./models/models");
 const app = express();
+app.use(logger("dev"));
 
-app.use(logger('dev'));
-
-app.use(express.urlencoded({
-  extended: true
-}));
-
-
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 
-app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname + '/public/index.html'));
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-app.get('/exercise', (req,res) => {
-  res.sendFile(path.join(__dirname + '/public/exercise.html'));
-});
 
-app.get('api/workouts', (req,res) => {
-  db.Workout.find({}).then(data => {
-    res.json(data);
-  });
-});
+// file routes
 
-app.get('/stats', (req,res) => {
-  res.sendFile(path.join(__dirname + '/public/stats.html'));
-});
 
-app.get('/api/workouts/range', (req,res) => {
-  
-  db.Workout.find({}).then(data => {
-    console.log(data);
-    res.json(data);
-  }).catch(err => {
-   
-    res.json(err);
+  app.get("/", (_req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
   })
-})
 
-app.put("/api/workouts/:id", (req, res) => {
-  db.Workout.findByIdAndUpdate(
-  req.params.id,
-    {
-      $push: {
-      exercises: req.body
-      }
-    }
-  ).then(data => {
-    res.json(data)
+  app.get("/exercise", (_req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'exercise.html'));
+  })
+
+  app.get('/stats', (_req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'stats.html'))
+  })
+
+
+  // api/workouts
+  
+
+  app.get("/api/workouts", (_, res) => {
+    User.find()
+      .then(result => {
+        res.json(result);
+      })
+      .catch(err => {
+        res.json(err);
+      });
   });
-}); 
-
-
-app.post("/api/workouts", ({body}, res) => {
-  db.Workout.create(body).then(data => {
-    res.json(data)
+ 
+  app.post("/api/workouts", (_, res) => {
+    User.create({})
+      .then(result => {
+        res.json(result);
+      })
+      .catch(err => {
+        res.json(err);
+      });
   });
-});
+  
+  app.put("/api/workouts/:id", ({body, params}, res) => {
+    User.findByIdAndUpdate(params.id, 
+      {$push:{exercises:body}}, 
+      {new:true, runValidators: true})
+      .then(result => {
+        res.json(result);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+ 
+// api/workouts/range 
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDb")
+
+  app.get('/api/workouts/range', (_, res) => {
+    User.find()
+      .then(result => {
+        res.json(result);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+ 
 
 app.listen(PORT, () => {
-  console.log(`APP is running on local host 8080`);
+  console.log(`App running on port ${PORT}!`);
 });
